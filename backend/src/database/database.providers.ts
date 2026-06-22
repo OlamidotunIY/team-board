3493d47fc0cf808b97a8fd9d99ad33e04546897b
OlamidotunIY <olamidotun225@gmail.com>
2026-06-22 19:48:57 +0100

    feat: add Docker configuration and environment setup for backend

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -1,10 +1,11 @@
-
 import * as mongoose from 'mongoose';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+      mongoose.connect(
+        process.env.MONGODB_URI ?? 'mongodb://localhost:27017/nest',
+      ),
   },
 ];
